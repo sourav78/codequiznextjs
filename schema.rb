12f1c097550095a2a4e35631bb258eef56c87984
sourav78 <souravranjan488@gmail.com>
2025-05-12 20:09:37 +0530

    schema.rb file 2

--- schema.rb
+++ schema.rb
@@ -1,36 +1,33 @@
-class CreateUsersAndUserInfos < ActiveRecord::Migration[7.0]
-  def change
-    create_table :users, id: :uuid do |t|
-      t.string  :user_name,         null: false, limit: 50,  index: { unique: true }
-      t.string  :email,             null: false, limit: 255, index: { unique: true }
-      t.string  :password,                          limit: 50
-      t.boolean :is_admin,          null: false, default: false
-      t.boolean :is_verified,       null: false, default: false
-      t.string  :verification_code,               limit: 6
-      t.timestamps
-    end
+ActiveRecord::Schema[8.0].define(version: 2025_05_12_120000) do
+  # These extensions must be enabled
+  enable_extension "plpgsql"
 
-    create_table :user_infos, id: :uuid do |t|
-      t.uuid    :user_id,          null: false, index: { unique: true }
-      t.string  :first_name,        null: false, limit: 50
-      t.string  :last_name,                    limit: 50
-      t.text    :bio
-      t.string  :profile_picture,              limit: 255
-      t.date    :dob
-      t.string  :country,                   limit: 50
-      t.timestamps
-    end
-
-    add_foreign_key :user_infos, :users, column: :user_id, primary_key: :id
+  create_table "users", id: :uuid, force: :cascade do |t|
+    t.string   "user_name",       null: false, limit: 50
+    t.string   "email",           null: false, limit: 255
+    t.string   "password",                      limit: 50
+    t.boolean  "is_admin",        null: false, default: false
+    t.boolean  "is_verified",     null: false, default: false
+    t.string   "verification_code",             limit: 6
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
-end
 
-# app/models/user.rb
-class User < ApplicationRecord
-  has_one :user_info, dependent: :destroy
-end
+  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true
+  add_index "users", ["email"],     name: "index_users_on_email",     unique: true
 
-# app/models/user_info.rb
-class UserInfo < ApplicationRecord
-  belongs_to :user
+  create_table "user_infos", id: :uuid, force: :cascade do |t|
+    t.uuid     "user_id",         null: false
+    t.string   "first_name",      null: false, limit: 50
+    t.string   "last_name",                   limit: 50
+    t.text     "bio"
+    t.string   "profile_picture",            limit: 255
+    t.date     "dob"
+    t.string   "country",                     limit: 50
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "user_infos", ["user_id"], name: "index_user_infos_on_user_id", unique: true
+  add_foreign_key "user_infos", "users"
 end
